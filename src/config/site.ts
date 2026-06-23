9e37e2edb77f4853f269193e71cd2023cc09ed9f
Szymon <Szymon.Cygan.FS@gmail.com>
2026-06-23 11:53:19 +0200

    Date

--- src/config/site.ts
+++ src/config/site.ts
@@ -4,7 +4,7 @@ export const siteConfig = {
   dates: "14 – 18 September 2026",
   location: "Będlewo Conference Center, Poland",
   contactEmail: "xivforum@impan.pl",
-  lastUpdate: "April 2026",
+  lastUpdate: "June 2026",
   copyright: "2026 XIV Forum of Partial Differential Equations",
   
   // Layout & UI
